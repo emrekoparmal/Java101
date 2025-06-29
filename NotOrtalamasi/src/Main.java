eb910a9a0c5cbb2dc98f0fd8b8a87942f5750b4b
CanBeyes <can@beyes.com.tr>
2025-06-29 15:44:21 +0300

    f

--- NotOrtalamasi/src/Main.java
+++ NotOrtalamasi/src/Main.java
@@ -1,8 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int not=0;
-        System.out.println("EmreKoparmal");
+      // Değişkenleri Oluştur
+        int mat,fizik,kimya,tarih,muzik,turkce;
+         // Scanner sınıfımızı tanımladık
+     Scanner inp = new Scanner(System.in) ;
+      // Kullanıcıdan değer alma
+        System.out.print("Matematik Notunuz : ");
+        mat = inp.nextInt() ;
+
+        System.out.print("Fizik  Notunuz : ");
+        fizik = inp.nextInt() ;
+
+        System.out.print("Kimya Notunuz : ");
+        kimya = inp.nextInt() ;
+
+        System.out.print("Muzik Notunuz : ");
+        muzik = inp.nextInt() ;
+
+        System.out.print("Tarih Notunuz : ");
+        tarih = inp.nextInt() ;
+
+        System.out.print("Türkçe Notunuz : ");
+        turkce = inp.nextInt() ;
+
+        int toplam= (mat + tarih + turkce + fizik + kimya + muzik) ;
+        double sonuc = toplam /6.0 ;
+        System.out.println("Ortalamanız : " + sonuc);
+
     }
 }
