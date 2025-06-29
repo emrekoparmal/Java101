bece7f48a47dcd164103c053d1a631588b2886ce
CanBeyes <can@beyes.com.tr>
2025-06-29 14:58:41 +0300

    a

--- NotOrtalamasi/src/Main.java
+++ NotOrtalamasi/src/Main.java
@@ -3,6 +3,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 5; i++) System.out.println("i = " + i);
+        System.out.println("EmreKoparmal");
     }
 }
