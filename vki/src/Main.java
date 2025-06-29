8972332d2ca622cac0c42fdcf44840ca86d44f54
CanBeyes <can@beyes.com.tr>
2025-06-29 20:31:30 +0300

    j

--- vki/src/Main.java
+++ vki/src/Main.java
@@ -13,8 +13,18 @@ public class Main {
         double indeks = kilo / (boy * boy);
 
         System.out.println("Vücut Kitle İndeksiniz :" + indeks);
+        if (indeks<18.5){
+            System.out.println("zayıf");
+        } else if (indeks<24.9) {
+            System.out.println("normal");
+        } else if (indeks<29.9) {
+            System.out.println("kilolu");
 
+        } else  {
+            System.out.println("OBEZ");
+
+        }
 
 
     }
-    }
+}
