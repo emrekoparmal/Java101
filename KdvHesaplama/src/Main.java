f5584f60cc1f9ef06525b83311b2caf4a6c1fa61
CanBeyes <can@beyes.com.tr>
2025-06-29 15:00:44 +0300

    a

--- KdvHesaplama/src/Main.java
+++ KdvHesaplama/src/Main.java
@@ -2,6 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) System.out.println("i = " + i);
+        System.out.println("KDV Hesabı");
     }
 }
