ac03e4df385ec059826b4eab4aca4a0cf0ca434d
CanBeyes <can@beyes.com.tr>
2025-06-29 16:58:04 +0300

    p

--- DikUcgenHipotenus/src/Main.java
+++ DikUcgenHipotenus/src/Main.java
@@ -1,7 +1,17 @@
-
+import javax.swing.*;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        int a,b;
+        double c;
+       Scanner girdi = new  Scanner(System.in);
+        System.out.println("Birinci Kenarı Giriniz : ");
+        a = girdi.nextInt();
+        System.out.println("İkinci Kenarı Giriniz : ");
+        b = girdi.nextInt();
 
-        System.out.println("Dik Üçgende Hipötenüs!");
+        c = Math.sqrt((a*a) + (b*b));
+        System.out.println("Üçgenin Hipotenüsü :" + c);
+
     }
 }
