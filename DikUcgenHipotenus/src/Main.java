7b89519eb7620bc2ec75db2d87479b9c83df5b13
CanBeyes <can@beyes.com.tr>
2025-06-29 15:03:57 +0300

    a

--- DikUcgenHipotenus/src/Main.java
+++ DikUcgenHipotenus/src/Main.java
@@ -2,6 +2,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Dik Üçgende Hipötenüs!");
+        System.out.println("Dik Üçgende Hipötenüs!");
     }
 }
