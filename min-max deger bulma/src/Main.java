7fdb49bff7e85c1b93510e39fa6c4d439a1e11cc
MertKoparmal <59117601+mertkoparmal08@users.noreply.github.com>
2025-07-29 22:21:26 +0300

    a

---
+++ min-max deger bulma/src/Main.java
@@ -0,0 +1,32 @@
+ import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Kaç sayı gireceksiniz : ");
+        int a= input.nextInt();
+
+        System.out.print("1. sayı: ");
+        int x= input.nextInt();
+        int min=x;
+        int max=x;
+
+        for (int i = 2; i <= a; i++) {
+            System.out.print(i+ ". sayı: ");
+            x= input.nextInt();
+
+            if(x>max){
+                max=x;
+            }
+            if(x<min){
+                min=x;
+            }
+        }
+
+        System.out.println("Min: "+ min);
+        System.out.println("Max: "+ max);
+    }
+}
+
+
+
